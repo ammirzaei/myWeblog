5c3b23dcf70baa6a3b77cb0159d5e94bcda1c170
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2022-12-11 22:19:31 +0330

    mongodb connected

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const dotEnv = require('dotenv');
+const morgan = require('morgan');
 
+const connectDB = require('./config/db');
 const homeRoutes = require('./routes/home');
 
 // congif env
-dotEnv.config({path : './config/config.env'});
+dotEnv.config({ path: './config/config.env' });
+
+// Database Connection
+connectDB();
 
 // initialize app
 const app = new express();
+
+// Logger
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 // View engine
 app.set('view engine', 'ejs');
---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB Connected : ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
