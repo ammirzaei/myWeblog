b54b8f07fce672b005450e35813ad838c0cd3bd5
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2022-12-18 13:03:32 +0330

    create user model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'نام کامل اجباری است'],
+        trim: true,
+        minlength: [10, 'حداقل باید 10 کاراکتر وارد کنید'],
+        maxLength: [150, 'حداکثر باید 150 کاراکتر وارد کنید']
+    },
+    email: {
+        type: String,
+        required: [true, 'ایمیل اجباری است'],
+        unique: [true, 'ایمیل تکراری است']
+    },
+    password: {
+        type: String,
+        required: [true, 'رمز عبور اجباری است'],
+        minlength: [8, 'حداقل باید 8 کاراکتر وارد کنید'],
+        maxLength: [150, 'حداکثر باید 150 کاراکتر وارد کنید']
+    },
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
