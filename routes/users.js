0e60c9ae52683dcaeca7d7efb55c6d253a37593a
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2022-12-19 13:25:16 +0330

    unistall yup and use fastest-validator for validation

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,10 @@
-const { Router } = require('express');
-const yup = require('yup');
-const router = new Router();
+const {registerSchema } = require('../schemas/usersSchema');
 
-// schema validators
-const registerSchema = yup.object().shape({
-    fullName: yup.string().required('نام و نام خانوادگی اجباری است').min(6,'نام و نام خانوادگی باید حداقل دارای 6 کاراکتر باشد').max(150,'نام و نام خانوادگی باید حداکثر دارای 150 کاراکتر باشد'),
-    email: yup.string().email('ایمیل وارد شده معتبر نیست').required('ایمیل اجباری است'),
-    password: yup.string().required('رمز عبور اجباری است').min(8,'رمز عبور باید حداقل دارای 8 کاراکتر باشد').max(150,'رمز عبور باید حداکثر دارای 150 کاراکتر باشد'),
-    repassword: yup.string().required('تکرار رمز عبور اجباری است').oneOf([yup.ref('password'), null],'رمز های عبور با هم یکسان نیست')
-});
+const { Router } = require('express');
+const Validator = require('fastest-validator');
+
+const router = new Router();
+const validator = new Validator();
 
 // Login Page
 router.get('/login', (req, res) => {
@@ -24,29 +20,38 @@ router.get('/register', (req, res) => {
     res.render('users/register', {
         pageTitle: 'صفحه ثبت نام',
         path: '/register',
-        layout: './layouts/usersLayout'
+        layout: './layouts/usersLayout',
+        errors : undefined
     });
 });
 
 // Register Handle
 router.post('/register', (req, res) => {
-    // const validator = registerSchema.isValid(req.body);
-    // validator.then((result) => {
-    //     if (result === true)
-    //         res.send('All Good');
-    //     else
-    //         res.send('error');
-    // });
-    registerSchema.validate(req.body).then(result=>{
+    const validate = validator.validate(req.body, registerSchema);
+    const errArr = [];
+    if (validate === true) {
+        const { fullName, email, password, repassword } = req.body;
+
+        // check equil pass
+        if (password !== repassword) {
+            errArr.push({ message: 'رمز های عبور یکسان نیستند' , field : 'repassword'});
+            return res.render('users/register', {
+                pageTitle: 'صفحه ثبت نام',
+                path: '/register',
+                layout: './layouts/usersLayout',
+                errors: errArr
+            });
+        }
+
         res.redirect('/login');
-    }).catch(err=>{
-        res.render('users/register',{
+    } else {
+        res.render('users/register', {
             pageTitle: 'صفحه ثبت نام',
             path: '/register',
             layout: './layouts/usersLayout',
-            errors : err.errors
-        });
-    });
+            errors: validate
+        })
+    }
 });
 
 module.exports = router;
