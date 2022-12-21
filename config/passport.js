b90d59c7a936621cd4332a873a50ef8b0f72efab
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2022-12-22 00:35:12 +0330

    config authentication with passportjs

---
+++ config/passport.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const { Strategy } = require('passport-local');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/userModel');
+
+passport.use(new Strategy({ 'usernameField': 'email' }, async (email, password, done) => {
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return done(null, false, {
+                message: 'کاربری با ایمیل وارد شده یافت نشد'
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            return done(null, user); // req.user
+        } else {
+            return done(null, false, { message: 'ایمیل یا رمز عبور صحیح نمی باشد' });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}));
+
+passport.serializeUser((user, done) => {
+    return done(null, user);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        return done(err, user);
+    })
+});
