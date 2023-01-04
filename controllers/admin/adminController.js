42d787b15eca5c0f5e55087389c7ab14f219cd79
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-04 10:33:12 +0330

    convert date to shamsi with jalali-moment module

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -1,19 +1,20 @@
 const Blog = require('../../models/blogModel');
-
+const { formatDate } = require('../../utils/jalali');
 module.exports.getDashboard = async (req, res) => {
-   try {
-    const blogs = await Blog.find({
-        user : req.user.id
-    });
-    res.render('admin/dashboard', {
-        pageTitle: 'صفحه داشبورد',
-        path: '/dashboard',
-        layout: './layouts/adminlayout',
-        fullName : req.user.fullName,
-        blogs 
-    });
-   } catch (err) {
-    console.log(err);
-   }
-   
+    try {
+        const blogs = await Blog.find({
+            user: req.user.id
+        });
+        res.render('admin/dashboard', {
+            pageTitle: 'صفحه داشبورد',
+            path: '/dashboard',
+            layout: './layouts/adminlayout',
+            fullName: req.user.fullName,
+            blogs,
+            formatDate
+        });
+    } catch (err) {
+        console.log(err);
+    }
+
 }
---
+++ utils/jalali.js
@@ -0,0 +1,5 @@
+const moment = require('jalali-moment');
+
+module.exports.formatDate = date =>{
+    return moment(date).locale('fa').format('HH:mm:ss - DD MMM YYYY')
+}
