d1f68294cbf155f5c821e2ca7e8c06aeb3602bf2
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-25 18:08:43 +0330

    added pagination blogs on home index

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -6,13 +6,30 @@ const { get500 } = require('./errorController');
 // Home -- GET
 module.exports.getHome = async (req, res) => {
     try {
-        const blogs = await Blog.find({ status: 'عمومی' }).sort({ createdAt: 'desc' }); // get all blogs(desc)
+        const pageId = +req.query.pageId || 1;
+        const blogPerPage = 2;
+
+        const countBlogs = await Blog.count({status: 'عمومی'});
+         // get all blogs(desc)
+        const blogs = await Blog.find({ status: 'عمومی' }).sort({ createdAt: 'desc' })
+             .skip((pageId - 1) * blogPerPage)
+             .limit(blogPerPage);
+        
+        const pagination = {
+            currentPage: pageId,
+            nextPage: pageId + 1,
+            previousPage: pageId - 1,
+            hasNextPage: (pageId * blogPerPage) < countBlogs,
+            hasPreviousPage: pageId > 1,
+            lastPage: Math.ceil(countBlogs / blogPerPage)
+        };
 
         res.render('home/index', {
             pageTitle: 'وبلاگ',
             path: '/',
             blogs,
-            formatDate
+            formatDate,
+            pagination
         });
     } catch (err) {
         console.log(err);
