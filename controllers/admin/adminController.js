d2e51b7285fece4ef2060c25fb87b5d6a046503d
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-25 11:32:56 +0330

    feat : added paginaton on dashboard page

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -15,16 +15,33 @@ const rootDir = require('../../utils/rootDir');
 // Dashboard Page -- GET
 module.exports.getDashboard = async (req, res) => {
     try {
+        const pageId = +req.query.pageId || 1; // aceess to the page id for paging
+        const blogPerPage = 2; // limit blog
+
+        const countBlogs = await Blog.count({ user: req.user.id }); // get count blog user
+
         const blogs = await Blog.find({
             user: req.user.id
-        });
+        }).skip((pageId - 1) * blogPerPage)
+            .limit(blogPerPage);
+
+        const pagination = {
+            currentPage: pageId,
+            nextPage: pageId + 1,
+            previousPage: pageId - 1,
+            hasNextPage: (pageId * blogPerPage) < countBlogs,
+            hasPreviousPage: pageId > 1,
+            lastPage: Math.ceil(countBlogs / blogPerPage)
+        };
+        
         res.render('admin/dashboard', {
             pageTitle: 'صفحه داشبورد',
             path: '/dashboard',
             layout: './layouts/adminlayout',
             fullName: req.user.fullName,
             blogs,
-            formatDate
+            formatDate,
+            pagination
         });
     } catch (err) {
         console.log(err);
