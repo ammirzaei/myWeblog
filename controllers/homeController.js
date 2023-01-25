ef48153a2f5f9e7467a0de339d720c43bba0bd19
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-25 17:19:57 +0330

    show all blogs on home index

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,21 @@
+const Blog = require('../models/blogModel');
+const { formatDate } = require('../utils/jalali');
+const { get500 } = require('./errorController');
+
+
 // Home -- GET
-module.exports.getHome = (req,res)=>{
-    res.render('home/index', {
-        pageTitle: 'وبلاگ',
-        path: '/'
-    });
+module.exports.getHome = async (req, res) => {
+    try {
+        const blogs = await Blog.find({ status: 'عمومی' }).sort({ createdAt: 'desc' }); // get all blogs(desc)
+
+        res.render('home/index', {
+            pageTitle: 'وبلاگ',
+            path: '/',
+            blogs,
+            formatDate
+        });
+    } catch (err) {
+        console.log(err);
+        get500(req, res);
+    }
 }
