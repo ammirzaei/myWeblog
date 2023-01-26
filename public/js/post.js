83daefa5812c28c0ad51fe1c95bacb24dd63ddb5
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-26 12:46:45 +0330

    added image preview on create and edit blog

--- public/js/post.js
+++ public/js/post.js
@@ -43,6 +43,16 @@ document.getElementById('btnUpload').addEventListener('click', function () {
 
 });
 
+document.getElementById('image').addEventListener('change', function () {
+    if (this.files[0]) {
+        const imagePreview = document.querySelector('#imagePreview');
+
+        imagePreview.style.display = 'block';
+
+        imagePreview.src = URL.createObjectURL(this.files[0]);
+    }
+});
+
 CKEDITOR.replace('body', {
     language: 'fa'
 });
