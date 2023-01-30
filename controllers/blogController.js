f2b547b73f29ea78f388c7a8a287eae041f998d9
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-30 11:29:38 +0330

    edit blogController for getting blogs

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,23 +1,16 @@
 const Blog = require('../models/blogModel');
-const { formatDate } = require('../utils/jalali');
 
 module.exports.getBlog = async (req, res) => {
     try {
         const blogID = req.params.id; // access with blog ID
 
-        const blog = await Blog.findById(blogID).populate('user'); // find blog with blog ID
+        const blog = await Blog.findOne({ id: blogID }).populate('user'); // find blog with blog ID
 
-        if (blog && blog.status === 'عمومی') {
-            res.render('blogs/blog', {
-                pageTitle: blog.title,
-                path: '/blog',
-                blog,
-                formatDate
-            });
-        } else
-            get404(req, res);
+        if (blog && blog.status === 'عمومی')
+            res.status(200).json({ blog });
+        else
+            res.status(404).json({ message: 'Not found blog', error: err });
     } catch (err) {
-        console.log(err);
-        get500(req, res);
+        res.status(500).json({ error: err });
     }
 }
