d05a10c836bc0165bc18ec24f280fb7cf6c142f5
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-31 14:52:02 +0330

    change comment blogRoute

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -3,7 +3,7 @@ const { Router } = require('express');
 const router = new Router();
 const blogController = require('../controllers/blogController');
 
-// Show Blog -- GET
+// Blog -- GET
 router.get('/blog/:id', blogController.getBlog);
 
 module.exports = router;
