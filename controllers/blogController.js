3b4883d121716fc913b44c7299632cb0ee22e57a
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-01-31 14:51:08 +0330

    use error handler to the blogController

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,16 +1,20 @@
 const Blog = require('../models/blogModel');
 
-module.exports.getBlog = async (req, res) => {
+module.exports.getBlog = async (req, res, next) => {
     try {
         const blogID = req.params.id; // access with blog ID
-
-        const blog = await Blog.findOne({ id: blogID }).populate('user'); // find blog with blog ID
-
+        
+        const blog = await Blog.findOne({ _id: blogID }).populate("user"); // find blog with blog ID
+       
         if (blog && blog.status === 'عمومی')
             res.status(200).json({ blog });
-        else
-            res.status(404).json({ message: 'Not found blog', error: err });
+        else {
+            const error = new Error('پستی پیدا نشد');
+            error.statusCode = 404;
+
+            throw error;
+        }
     } catch (err) {
-        res.status(500).json({ error: err });
+        next(err);
     }
 }
