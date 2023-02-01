84515c2588261622a54c4f181027e27e7f4aceeb
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-02-01 10:08:14 +0330

    update db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const debug = require('debug')('weblog');
 
 module.exports = async () => {
     try {
         mongoose.set('strictQuery', false);
         const conn = await mongoose.connect(process.env.MONGO_URI);
 
-        debug(`Mongodb Connected : ${conn.connection.host}`)
+        console.log(`Mongodb Connected : ${conn.connection.host}`)
     } catch (error) {
         console.log(error);
         process.exit(1);
